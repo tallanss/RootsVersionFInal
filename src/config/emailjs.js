8eb045caf478909a0f2d4287e5f0d75be012d841
tallanss <toutainallan@icloud.com>
2026-03-29 17:11:24 +0200

    google calendar

--- src/config/emailjs.js
+++ src/config/emailjs.js
@@ -6,10 +6,10 @@ export const BOOKING_CONFIG = {
   // ===== GOOGLE APPS SCRIPT =====
   // URL de votre Google Apps Script déployé en tant qu'Application Web
   // Suivez les instructions dans google-apps-script/booking-api.gs
-  APPS_SCRIPT_URL: '',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyMkUFYD6lM5l2E1eZ3qhgK8z9xBYoEOKFKeFgf4b-Hk47mBvgj42Quaug5SHn2QytHwA/exec',
 
   // ===== INFO ENTREPRISE =====
-  OWNER_EMAIL: 'contact@photoroots.fr',
+  OWNER_EMAIL: 'toutainallan@gmail.com',
   BUSINESS_NAME: 'PhotoRoots',
   BUSINESS_PHONE: '+33 6 12 34 56 78',
 };
